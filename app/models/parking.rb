acdd0c08a112bdfbdbd3b31a652712df2643d30b
atusi0314 <130933944+atusi0314@users.noreply.github.com>
2024-01-18 07:02:26 +0000

    [add]ログイン画面・ユーザー登録画面・ユーザー詳細画面のレイアウトを作成

--- app/models/parking.rb
+++ app/models/parking.rb
@@ -1,17 +1,17 @@
 class Parking < ApplicationRecord
-  has_one_attached :image
+  has_one_attached :parking_image
   belongs_to :user
   has_many :favorites, dependent: :destroy
 
   validates :spot_name, presence: true
-  validates :image, presence: true
 
-  def get_image
-    unless image.attached?
-      file_path = Rails.root.join('app/assets/images/no_image.jpg')
-      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+  
+  def get_parking_image(width, height)
+    unless parking_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image_parking.png')
+      parking_image.attach(io: File.open(file_path), filename: 'no_image_parking.png', content_type: 'image/png')
     end
-    image
+    parking_image.variant(resize_to_limit: [300, 300]).processed
   end
 
   def favorited_by?(user)
