14fc8d7cf9bfbeaae959c231edd864cc5facbca2
atusi0314 <130933944+atusi0314@users.noreply.github.com>
2024-02-06 08:27:32 +0000

    [add]stylesheets

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,16 +4,18 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-// import Turbolinks from "turbolinks"
+import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
 import "jquery";
 import "popper.js";
 import "bootstrap";
-import "../stylesheets/application.scss"; 
-import "../stylesheets/homes.scss.erb";
+import "../stylesheets/application"; 
 
 Rails.start()
-// Turbolinks.start()
+Turbolinks.start()
 ActiveStorage.start()
+
+import "../stylesheets/homes.scss.erb"
+import "../stylesheets/application.scss"
