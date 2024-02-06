ad2760ac62aa82cda8cd028cbdf5e6abbb5642ea
atusi0314 <130933944+atusi0314@users.noreply.github.com>
2024-02-06 07:22:39 +0000

    [replace]記述変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'maps/index'
-  devise_for :users
   root to: "homes#top"
+  devise_for :users
   resources :maps, only: [:index]
   resources :parkings, only: [:new, :create, :index, :show, :destroy] do
     resource :favorite, only:[:create, :destroy]
